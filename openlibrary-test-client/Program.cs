84274a0af6ec39a37fc81d154dc3d60a549bf170
enzotanga <enzo.tanga@onetree.com>
2021-10-15 18:26:27 -0300

    Add project files.

---
+++ openlibrary-test-client/Program.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace openlibrary_test_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            // Establecimiento del protocolo ssl de transporte
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            // Url de ejemplo
+            var mUrl = "http://openlibrary.org/search.json?q=the+lord+of+the+rings";
+
+            // Se crea el request http
+            HttpWebRequest mRequest = (HttpWebRequest)WebRequest.Create(mUrl);
+
+            try
+            {
+                // Se ejecuta la consulta
+                WebResponse mResponse = mRequest.GetResponse();
+
+                // Se obtiene los datos de respuesta
+                using (Stream responseStream = mResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+
+                    // Se parsea la respuesta y se serializa a JSON a un objeto dynamic
+                    dynamic mResponseJSON = JsonConvert.DeserializeObject(reader.ReadToEnd());
+
+                    System.Console.WriteLine("numFound: {0}", mResponseJSON.numFound);
+
+                    // Se iteran cada uno de los resultados.
+                    foreach (var bResponseItem in mResponseJSON.docs)
+                    {
+                        // De esta manera se accede a los componentes de cada item
+                        // Se decodifican algunos elementos HTML
+                        System.Console.WriteLine("Solo titulo -> {0}", HttpUtility.HtmlDecode(bResponseItem.title.ToString()));
+
+                        // Se muestra por pantalla cada item completo
+                        //System.Console.WriteLine("Item completo -> {0}", bResponseItem);
+
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse mErrorResponse = ex.Response;
+                using (Stream mResponseStream = mErrorResponse.GetResponseStream())
+                {
+                    StreamReader mReader = new StreamReader(mResponseStream, Encoding.GetEncoding("utf-8"));
+                    String mErrorText = mReader.ReadToEnd();
+
+                    System.Console.WriteLine("Error: {0}", mErrorText);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Error: {0}", ex.Message);
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
